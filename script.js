a37db7fa71fbc48085a85adca23271fd23307c8d
t-hanafin <thanafin@outlook.com>
2023-03-30 14:08:52 -0400

    add duplicate-checking validation to the addBookToLibrary function--won't add a second copy of a book now.

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-const container = document.querySelector('#container');
-const content = document.createElement('div');
-content.classList.add('content');
-let renderedRecord = '';
+let myLibrary = [
+    {
+        title:      'Americanah',
+        author:     'Chimamanda Ngozi Adichie',
+        pages:      402,
+        read:       true
+    }
+];
 
+const table = document.querySelector('#table');
+let row = document.createElement('tr');
+let cell1 = document.createElement('td');
+let cell2 = document.createElement('td');
+let cell3 = document.createElement('td');
+let cell4 = document.createElement('td');
+const bookForm = document.querySelector('#library_form');
 
-
-let myLibrary = [];
-
+addBookToLibrary('In The Skin of a Lion', 'Michael Ondaatje', 250, true);
+addBookToLibrary('Moon of the Crusted Snow', 'Waubgeshig Rice', 231, true);
+    
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read // True or false.
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read; // True or false.
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    let newTitle = title
-    let newAuthor = author
-    let newPages = pages
-    let newRead = read
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+    // Checks to see if the book-to-be-added is already in the library.
+    // Does not add book if it's already in the library.
+
+    myLibrary.some(book => book.title === title && book.author === author) ?
+        undefined :
+        myLibrary.push(newBook = new Book(title, author, pages, read));
 }
 
-addBookToLibrary('Americanah', 'Chimamanda Ngozi Adichie', 402, true);
-addBookToLibrary('In The Skin of a Lion', 'Michael Ondaatje', 250, true);
-
-console.table(myLibrary);
-
-const bookForm = document.querySelector('#library_form');
 
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -45,16 +50,8 @@ bookForm.addEventListener('submit', (e) => {
     updateDisplay();
 });
 
-const table = document.querySelector('#table');
-let row = document.createElement('tr');
-let cell1 = document.createElement('td');
-let cell2 = document.createElement('td');
-let cell3 = document.createElement('td');
-let cell4 = document.createElement('td');
-
-
 function updateDisplay() {
-
+    table.innerHTML = "";
     myLibrary.forEach(item => {
         var row = table.insertRow(-1);
         var cell1 = row.insertCell(0);
@@ -70,9 +67,7 @@ function updateDisplay() {
         row.appendChild(cell3);
         row.appendChild(cell4);
         table.appendChild(row);
-        console.log(cell1.innerText);
     })
 }
-
 
 updateDisplay();
