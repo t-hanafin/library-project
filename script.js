6b99b64450297c9beadb630bf465bf4321842d12
t-hanafin <thanafin@outlook.com>
2023-03-30 11:55:17 -0400

    add function updating the library, then displaying the contents of the library in a table at the bottom of the library HTML page

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('#container');
 const content = document.createElement('div');
 content.classList.add('content');
+let renderedRecord = '';
+
+
 
 let myLibrary = [];
 
@@ -20,25 +23,56 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
-addBookToLibrary('poo', 'pee', 9, false);
-addBookToLibrary('crap', 'fart', 10, false);
-addBookToLibrary('silly', 'fanonsenrt', 11, false);
-addBookToLibrary('nonsense', 'caca', 12, false);
-addBookToLibrary('foo', 'bar', 13, true);
-addBookToLibrary('duh', 'buh', 14, false);
+addBookToLibrary('Americanah', 'Chimamanda Ngozi Adichie', 402, true);
+addBookToLibrary('In The Skin of a Lion', 'Michael Ondaatje', 250, true);
 
 console.table(myLibrary);
 
-const testForm = document.querySelector('#library_form');
+const bookForm = document.querySelector('#library_form');
 
-testForm.onsubmit = (e) => {
+bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const formData = new FormData(testForm);
-  
-    console.log("Form Data");
-    for (let obj of formData) {
-        console.log(obj);
-        content.textContent = obj;
-        container.appendChild(content);
+    let formTitle = bookForm.elements[0].value;
+    let formAuthor = bookForm.elements[1].value;
+    let formPages = parseInt(bookForm.elements[2].value);
+    let formRead = bookForm.elements[3].value;
+    if (formRead === 'true') {
+        formRead = true;
+    } else {
+        formRead = false;
     }
-  };
+    addBookToLibrary(formTitle, formAuthor, formPages, formRead);
+    updateDisplay();
+});
+
+const table = document.querySelector('#table');
+let row = document.createElement('tr');
+let cell1 = document.createElement('td');
+let cell2 = document.createElement('td');
+let cell3 = document.createElement('td');
+let cell4 = document.createElement('td');
+
+
+function updateDisplay() {
+
+    myLibrary.forEach(item => {
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        cell1.textContent = (item[Object.keys(item)[0]]);
+        cell2.textContent = (item[Object.keys(item)[1]]);
+        cell3.textContent = (item[Object.keys(item)[2]]);
+        cell4.textContent = (item[Object.keys(item)[3]]);
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+        row.appendChild(cell3);
+        row.appendChild(cell4);
+        table.appendChild(row);
+        console.log(cell1.innerText);
+    })
+}
+
+
+updateDisplay();
